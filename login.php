d2504994b6b949be59d84e5f46f77f0e4451521c
hinata0801 <2401355@s.asojuku.ac.jp>
2025-10-27 12:20:22 +0900

    Update login.php

--- login.php
+++ login.php
@@ -5,15 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン画面</title>
+    <link rel="stylesheet" href="../bulma/css/bulma.min.css">
 </head>
 <body>
-    <h2>SSB</h2>
-    <h2>ログイン</h2>
-    <form action="home.php" method="post">
-    <p>メールアドレス<input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$"></p>
-    <p>パスワード<input type="password" name="password"required minlength="8"></p>
-    <button><a href="entry.php">新規登録</button>
-    <input type="submit" value="ログイン">
-    </form>
+    <div>
+        <h2 class="has-text-centered hsl(27, 82%, 51%)">SSB</h2>
+        <h2 class="has-text-centered hsl(27, 82%, 51%)">ログイン</h2>
+        <form action="home.php" method="post">
+        <p>メールアドレス</p>
+        <p><input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="text"></p>
+        <p>パスワード</p>
+        <p><input type="password" name="password"required minlength="8"></p>
+        <button><a href="entry.php">新規登録</button>
+        <input type="submit" value="ログイン">
+        </form>
+    </div>
 </body>
 </html>
