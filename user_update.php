0f167eb564d3d3d0bbad6e66d933daab81106b56
tomoya930 <2101031@s.asojuku.ac.jp>
2025-10-27 12:01:12 +0900

    Update user_update.php

--- user_update.php
+++ user_update.php
@@ -4,8 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ユーザー情報更新</title>
+    <P>登録済みの情報を更新できます</p>
 </head>
 <body>
-    
+     <form action="#" method="post">
+      <div class="field">
+        <label for="name">ユーザー名</label>
+        <input id="name" name="name" type="text" value="仮名前">
+      </div>
+
+      <div class="field">
+        <label for="email">メールアドレス</label>
+        <input id="email" name="email" type="email" value="仮メール@example.com">
+      </div>
+
+      <div class="field">
+        <label for="pass">電話番号</label>
+        <input id="pass" name="TEL" type="tel" placeholder="電話番号の入力">
+      </div>
+
+      <div class="field">
+        <label for="pass">新しいパスワード</label>
+        <input id="pass" name="password" type="password" placeholder="変更する場合のみ入力">
+      </div>
+
+      <button type="submit">更新する</button>
+
+      <div class="links">
+        <a href="#">キャンセル</a>
+      </div>
+    </form>
+  </main>
 </body>
 </html>
