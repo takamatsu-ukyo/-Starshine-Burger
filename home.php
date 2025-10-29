297f5bfba2f2753fcb02f1b17178aac6e00b2409
takamatsu-ukyo <2401357@s.asojuku.ac.jp>
2025-10-29 11:24:27 +0900

    Update home.php

--- home.php
+++ home.php
@@ -7,6 +7,19 @@
     <title>ホーム画面</title>
 </head>
 <body>
-    
+    <h2>SSB</h2>
+    <form method="POST" action="">
+    <input type="text" name="keyword" id="keywordInput" value="<?= htmlspecialchars($keyword) ?>" placeholder="商品名を入力">
+    <button type="submit">検索</button>
+    </form>
+    <?php
+    // キーワード取得（POST）
+    $keyword = isset($_POST['keyword']) ? $_POST['keyword'] : '';
+    ?>
+
+
+    <button>チーズ</button>
+    <button>てりやき</button>
+    <button>ポテト</button>
 </body>
 </html>
