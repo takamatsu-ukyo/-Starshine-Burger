91382e7a4e67331d712f2abff72e54e8b5a1feed
hinata0801 <2401355@s.asojuku.ac.jp>
2025-10-31 10:24:10 +0900

    Update login.php

--- login.php
+++ login.php
@@ -8,20 +8,6 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
 <body>
-<<<<<<< HEAD
-    <div>
-        <h2 class="has-text-centered hsl(27, 82%, 51%)">SSB</h2>
-        <h2 class="has-text-centered hsl(27, 82%, 51%)">ログイン</h2>
-        <form action="home.php" method="post">
-        <p>メールアドレス</p>
-        <p><input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="table-cell-border-color: #E5C287;"></p>
-        <p>パスワード</p>
-        <p><input type="password" name="password"required minlength="8" class="table-cell-border-color: #E5C287;></p>
-        <button><a href="entry.php" class="button is-danger" style="background-color: rgb(232, 117, 26); color: rgb(0, 0, 0);">新規登録</button>
-        <input type="submit" value="ログイン" class="button is-danger" style="background-color: rgb(232, 117, 26); color: rgb(0, 0, 0);">
-        </form>
-    </div>
-=======
   <div class="container mt-6">
     <h2 class="title has-text-centered" style="color: hsl(27, 82%, 51%)">SSB</h2>
     <h2 class="subtitle has-text-centered" style="color: hsl(27, 82%, 51%)">ログイン</h2>
@@ -51,6 +37,5 @@
       </div>
     </form>
   </div>
->>>>>>> main
 </body>
 </html>
