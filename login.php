68d29e2a8808ca8d97121347d0f075a886d1a98f
iseri-shuho <2401342@s.asojuku.ac.jp>
2025-10-31 09:48:15 +0900

    Update login.php

--- login.php
+++ login.php
@@ -1,24 +1,41 @@
-<?php require 'db-connect.php';?>
+<?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ログイン画面</title>
-    <link rel="stylesheet" href="../bulma/css/bulma.min.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ログイン画面</title>
+  <link rel="stylesheet" href="../bulma/css/bulma.min.css">
 </head>
 <body>
-    <div>
-        <h2 class="has-text-centered hsl(27, 82%, 51%)">SSB</h2>
-        <h2 class="has-text-centered hsl(27, 82%, 51%)">ログイン</h2>
-        <form action="home.php" method="post">
-        <p>メールアドレス</p>
-        <p><input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="text"></p>
-        <p>パスワード</p>
-        <p><input type="password" name="password"required minlength="8"></p>
-        <button><a href="entry.php">新規登録</button>
-        <input type="submit" value="ログイン">
-        </form>
-    </div>
+  <div class="container mt-6">
+    <h2 class="title has-text-centered" style="color: hsl(27, 82%, 51%)">SSB</h2>
+    <h2 class="subtitle has-text-centered" style="color: hsl(27, 82%, 51%)">ログイン</h2>
+
+    <form action="home.php" method="post" class="box">
+      <div class="field">
+        <label class="label">メールアドレス</label>
+        <div class="control">
+          <input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="input">
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">パスワード</label>
+        <div class="control">
+          <input type="password" name="password" required minlength="8" class="input">
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-centered">
+        <p class="control">
+          <a href="entry.php" class="button is-link is-light">新規登録</a>
+        </p>
+        <p class="control">
+          <input type="submit" value="ログイン" class="button is-primary">
+        </p>
+      </div>
+    </form>
+  </div>
 </body>
 </html>
