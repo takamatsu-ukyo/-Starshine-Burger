ca19d880ccb020cf8dcf101580fd01c0872434ff
hinata0801 <2401355@s.asojuku.ac.jp>
2025-10-31 10:12:21 +0900

    Update login.php

--- login.php
+++ login.php
@@ -13,11 +13,11 @@
         <h2 class="has-text-centered hsl(27, 82%, 51%)">ログイン</h2>
         <form action="home.php" method="post">
         <p>メールアドレス</p>
-        <p><input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="text"></p>
+        <p><input type="email" name="mail" required pattern="^[a-zA-Z0-9._%+-]+@example\.com$" class="table-cell-border-color: #E5C287;"></p>
         <p>パスワード</p>
-        <p><input type="password" name="password"required minlength="8"></p>
-        <button><a href="entry.php">新規登録</button>
-        <input type="submit" value="ログイン">
+        <p><input type="password" name="password"required minlength="8" class="table-cell-border-color: #E5C287;></p>
+        <button><a href="entry.php" class="button is-danger" style="background-color: rgb(232, 117, 26); color: rgb(0, 0, 0);">新規登録</button>
+        <input type="submit" value="ログイン" class="button is-danger" style="background-color: rgb(232, 117, 26); color: rgb(0, 0, 0);">
         </form>
     </div>
 </body>
