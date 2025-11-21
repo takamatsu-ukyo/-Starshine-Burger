76fe98e3b8e1c7321e2b787d7ce2a9c75888bd72
takamatsu-ukyo <2401357@s.asojuku.ac.jp>
2025-11-21 10:10:44 +0900

    Update buy_completed.php

--- buy_completed.php
+++ buy_completed.php
@@ -59,8 +59,12 @@ unset($_SESSION['cart'], $_SESSION['quantity']);
   <!-- ④ 注文内容 -->
   <h3>ご注文内容</h3>
   <?php foreach ($cart as $item): ?>
-    <p><?= htmlspecialchars($item['food_name']) ?> × <?= $item['quantity'] ?>個 ¥<?= number_format($item['price'] * $item['quantity']) ?></p>
-  <?php endforeach; ?>
+      <?php if ($item['quantity'] > 0): ?>
+        <p>
+          <?= htmlspecialchars($item['food_name']) ?> × <?= $item['quantity'] ?>個 ¥<?= number_format($item['price'] * $item['quantity']) ?>
+        </p>
+      <?php endif; ?>  
+    <?php endforeach; ?>
 
   <!-- ⑤ 合計金額 -->
   <h3>合計：¥<?= number_format($total) ?></h3>
